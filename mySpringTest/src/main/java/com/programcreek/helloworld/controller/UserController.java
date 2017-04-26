89b2ca416603a855721e2c9522fa5f6b70f0e199
fengshuguang <fengshuguang@meituan.com>
2017-04-26 16:44:25 +0800

    fix and connection to db with mybatis connection

--- mySpringTest/src/main/java/com/programcreek/helloworld/controller/UserController.java
+++ mySpringTest/src/main/java/com/programcreek/helloworld/controller/UserController.java
@@ -1,34 +1,22 @@
-package com.programcreek.helloworld.controller;
+package com.programcreek.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
-public class UserController extends MultiActionController {
-	
-	public ModelAndView home(HttpServletRequest request, HttpServletResponse response){
-		ModelAndView model = new ModelAndView("user");
-		model.addObject("message", "home 方法");
-		return model;
-	}
-	
-	public ModelAndView add(HttpServletRequest request, HttpServletResponse response){
-		ModelAndView model = new ModelAndView("user");
-		model.addObject("message", "add 方法");
-		return model;
-	}
-	
-	public ModelAndView remove(HttpServletRequest request, HttpServletResponse response){
-		ModelAndView model = new ModelAndView("user");
-		model.addObject("message", "remove 方法");
-		return model;
-	}
-	
-	public ModelAndView main(HttpServletRequest request, HttpServletResponse response){
-		ModelAndView model = new ModelAndView("user");
-		model.addObject("message", "main 方法");
+import com.programcreek.dao.UserMapper;
+import com.programcreek.model.UserExample;
+
+@Controller
+public class UserController {
+
+	UserMapper userMapper;
+	@RequestMapping("getTotalNumber")
+	public ModelAndView getNumber(){
+		ModelAndView model = new ModelAndView("userController");
+		UserExample example = new UserExample();
+		userMapper.countByExample(example);
+		model.addObject("number", 0);
 		return model;
 	}
 }
---
+++ mySpringTest/src/main/java/com/programcreek/helloworld/controller/UserControllerTest.java
@@ -0,0 +1,34 @@
+package com.programcreek.helloworld.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class UserController extends MultiActionController {
+	
+	public ModelAndView home(HttpServletRequest request, HttpServletResponse response){
+		ModelAndView model = new ModelAndView("user");
+		model.addObject("message", "home 方法");
+		return model;
+	}
+	
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response){
+		ModelAndView model = new ModelAndView("user");
+		model.addObject("message", "add 方法");
+		return model;
+	}
+	
+	public ModelAndView remove(HttpServletRequest request, HttpServletResponse response){
+		ModelAndView model = new ModelAndView("user");
+		model.addObject("message", "remove 方法");
+		return model;
+	}
+	
+	public ModelAndView main(HttpServletRequest request, HttpServletResponse response){
+		ModelAndView model = new ModelAndView("user");
+		model.addObject("message", "main 方法");
+		return model;
+	}
+}
